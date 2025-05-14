540edc2777a9009042431ab8ae76647ac2d0d1f5
lilya <lilya@10.127.49.159>
2025-05-14 09:39:05 -0400

    finished level class

--- src/Level.java
+++ src/Level.java
@@ -10,8 +10,14 @@
 public class Level {
     private int score;
     private static int highestScore = 0;
+    private String answers;
     
-    public Level(){
+        public Level(String answers){//constructor 1
+        this.score = 0;
+        this.answers = answers;
+    }
+        
+    public Level(){//consctructor 2
         this.score = 0;
     }
     
@@ -19,8 +25,30 @@ public class Level {
         return text;
     }
     
-    public 
+    public boolean checkAnswer(String userAns, int score){
+        boolean isTrue = false;
+        if (userAns.equals(answers)){
+            isTrue = true;
+            this.score += score;
+        }
+        return isTrue;
+    }
     
+    public int getScore(){
+        return score;
+    }
     
+    public void setScore(int score){
+        this.score = score;
+    }
     
+public static void updateHighestScore(int score) {
+    if (score > highestScore) {
+        highestScore = score;
+    }
+}
+
+public static int getHighestScore() {
+    return highestScore;
+}
 }
