3d79877f45c65bc039ede887bca267f1af296af0
lilya <lilya@10.127.49.159>
2025-05-14 09:11:01 -0400

    started level class

---
+++ src/Level.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lilya
+ */
+public class Level {
+    private int score;
+    private static int highestScore = 0;
+    
+    public Level(){
+        this.score = 0;
+    }
+    
+    public String displayText(String text){
+        return text;
+    }
+    
+    public 
+    
+    
+    
+}
